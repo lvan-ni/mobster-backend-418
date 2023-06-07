7b1ff4ec222e25c1c9f49d5128d16e05b25dfb57
gnilvan <gnilvan@users.noreply.github.com>
2023-06-07 17:44:18 +0200

    mob post setup

--- routes/index.js
+++ routes/index.js
@@ -41,12 +41,21 @@ router.get('/mobs', function (req, res, next) {
 
 //  add a new mob
 router.post('/mobs', function (req, res, next) {
-  res.send('mobs');
+  const newMob = {
+    id: mobs.length + 1,
+    name: req.body.name
+  };
+
+  mobs.push(newMob);
+
+  res.status(201).setHeader('location', `/mobs/${newMob.id}`).json(mobs);
+
 });
 
 // get a particular mob
 router.get('/mobs/:mobId', function (req, res, next) {
-  res.send('mobs');
+  const id = req.params.id;
+  res.json(mobs[id - 1]);
 });
 
 // get all mob members of a particular mob
