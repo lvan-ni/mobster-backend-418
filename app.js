5f1a138abc716558d7e1bc92f42e8c72436d8035
gnilvan <gnilvan@users.noreply.github.com>
2023-06-08 23:21:15 +0200

    cors implemented

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+  }),
+);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,10 @@ const mobs = [
     mobId: 3,
     mobName: 'LOGA',
   },
+  {
+    mobId: 4,
+    mobName: 'JAR',
+  }
 ];
 
 const members = [
