3519d7d7182937dd0dec8e97fab0b8787d25676d
gnilvan <gnilvan@users.noreply.github.com>
2023-06-08 23:24:29 +0200

    gh pages cors added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173'], ['https://lvan-ni.github.io/mobster-frontend-418/'],
   }),
 );
 
